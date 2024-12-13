9d0553b0cf379b33116d86caed3215b9f82fcda8
yashksaini-coder <ys3853428@gmail.com>
2024-12-13 17:10:21 +0530

    feat: 🎸 Room ID or code generator function

---
+++ lib/utils.ts
@@ -0,0 +1,10 @@
+export function generateRoomCode(): string {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters[randomIndex];
+    }
+    return code;
+}
+
